6ad48af20bcfdec80ea534a8ed6a84070a3cc773
sergeivrt <sergeivrt@gmail.com>
2024-02-20 11:17:51 +0200

    push

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,16 +1,21 @@
 describe("Issue Deletion", () => {
-  
   beforeEach(() => {
     cy.visit("/");
-    cy.url().should("eq", `${Cypress.env("baseUrl")}project`).then((url) => {
-      cy.visit(url + "/board");
-      cy.contains("This is an issue of type: Task.").click();
-    });
+    cy.url()
+      .should("eq", `${Cypress.env("baseUrl")}project`)
+      .then((url) => {
+        cy.visit(url + "/board");
+        cy.contains("This is an issue of type: Task.").click();
+      });
   });
 
   it.only("Should delete an issue", () => {
     getIssueDetailsModal().within(() => {
       cy.get('[data-testid="modal:issue-details"]').should("be.visible");
+      cy.get('[data-testid="icon:trash"]').click();
+      cy.get('[data-testid="modal:confirm"]').click();
+      cy.reload();
+      cy.contains("This is an issue of type: Task.").should('not.exist');
     });
   });
 });
