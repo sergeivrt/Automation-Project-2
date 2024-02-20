c7cdf1849306abef1f271f173dd1822778e771b3
sergeivrt <sergeivrt@gmail.com>
2024-02-20 12:53:41 +0200

    push

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -10,17 +10,17 @@ describe("Issue Deletion", () => {
   });
 
   it("Should cancel deletition of an issue", () => {
-      cy.get('[data-testid="modal:issue-details"]').should("be.visible");
-      cy.get('[data-testid="icon:trash"]').click();
-      cy.get('button.sc-bwzfXH.ewzfNn.sc-kGXeez.bLOzZQ').click();
-      cy.get('[data-testid="icon:close"]').first().click();
-      cy.contains("This is an issue of type: Task.").should('exist');
-    });
-
-    it.only("Should delete an issue", () => {
-      cy.get('[data-testid="modal:issue-details"]').should("be.visible");
-      cy.get('[data-testid="icon:trash"]').click();
-      cy.get('[data-testid="modal:confirm"]').click();
-      cy.contains("This is an issue of type: Task.").should('not.exist');
-    });
+    cy.get('[data-testid="modal:issue-details"]').should("be.visible");
+    cy.get('[data-testid="icon:trash"]').click();
+    cy.get("button.sc-bwzfXH.ewzfNn.sc-kGXeez.bLOzZQ").click();
+    cy.get('[data-testid="icon:close"]').first().click();
+    cy.contains("This is an issue of type: Task.").should("exist");
   });
+
+  it.only("Should delete an issue", () => {
+    cy.get('[data-testid="modal:issue-details"]').should("be.visible");
+    cy.get('[data-testid="icon:trash"]').click();
+    cy.get("button.sc-bwzfXH.dIxFno.sc-kGXeez.bLOzZQ").click();
+    cy.contains("This is an issue of type: Task.").should("not.exist");
+  });
+});
