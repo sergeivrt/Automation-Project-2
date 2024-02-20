d9a7c63ffb67179213a4beb9c58d44d8b7b1917c
sergeivrt <sergeivrt@gmail.com>
2024-02-20 10:59:51 +0200

    Update issue-delete.cy.js

--- cypress/e2e/issue-delete.cy.js
+++ cypress/e2e/issue-delete.cy.js
@@ -1,35 +1,16 @@
-import IssueModal from "../pages/IssueModal";
-
 describe("Issue Deletion", () => {
+  
   beforeEach(() => {
     cy.visit("/");
-    cy.url()
-      .should("eq", `${Cypress.env("baseUrl")}project`)
-      .then((url) => {
-        cy.visit(url + "/board");
-        cy.contains("This is an issue of type: Task.").click();
-      });
+    cy.url().should("eq", `${Cypress.env("baseUrl")}project`).then((url) => {
+      cy.visit(url + "/board");
+      cy.contains("This is an issue of type: Task.").click();
+    });
   });
 
-  it("Should delete an issue", () => {
-    IssueModal.getIssueDetailModal().should("be.visible");
-    IssueModal.clickDeleteButton();
-    IssueModal.confirmDeletion();
-    IssueModal.validateIssueVisibilityState(
-      "This is an issue of type: Task.",
-      false
-    );
-  });
-
-  it("Should delete an issue and cancel Confirmation", () => {
-    IssueModal.getIssueDetailModal().should("be.visible");
-    IssueModal.clickDeleteButton();
-    IssueModal.cancelDeletion();
-    cy.get('[data-testid="modal:confirmation-dialog"]').should("not.exist");
-    IssueModal.closeDetailModal();
-    IssueModal.validateIssueVisibilityState(
-      "This is an issue of type: Task.",
-      true
-    );
+  it.only("Should delete an issue", () => {
+    getIssueDetailsModal().within(() => {
+      cy.get('[data-testid="modal:issue-details"]').should("be.visible");
+    });
   });
 });
