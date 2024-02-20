1e0689ea3d91230b5e79a923d5af38fe9541ec3d
sergeivrt <sergeivrt@gmail.com>
2024-02-20 17:48:42 +0200

    push

--- cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
+++ cypress/e2e/e2e-pom/issue-delete-in-pom.cy.js
@@ -7,20 +7,19 @@ describe("Issue delete", () => {
       .should("eq", `${Cypress.env("baseUrl")}project/board`)
       .then((url) => {
         cy.contains(issueTitle).click();
+        IssueModal.getIssueDetailModal().should("be.visible");
       });
   });
 
   const issueTitle = "This is an issue of type: Task.";
 
   it("Should delete issue successfully", () => {
-    IssueModal.getIssueDetailModal().should("be.visible");
     IssueModal.clickDeleteButton();
     IssueModal.confirmDeletion();
     IssueModal.validateIssueVisibilityState(issueTitle, false);
   });
 
   it("Should cancel deletion process successfully", () => {
-    IssueModal.getIssueDetailModal().should("be.visible");
     IssueModal.clickDeleteButton();
     IssueModal.cancelDeletion();
     cy.get('[data-testid="modal:confirmation-dialog"]').should("not.exist");
